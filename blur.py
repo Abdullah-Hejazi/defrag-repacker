ea11e4e83753c08f1c1aa600fb2dcb0c166cfe02
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-12-19 16:32:32 +0200

    make nude and safe pictures visible after process

--- blur.py
+++ blur.py
@@ -7,17 +7,22 @@ import zipfile
 
 SENSITIVITY = 3
 
-def blur_folder(input):
+def blur_folder(input, output):
     for subdir, dirs, files in os.walk(input):
         for file in files:
             filePath = os.path.join(subdir, file)
+
+            outputPath = filePath.replace(input, output)
 
             print("Processing: " + filePath)
 
             OriImage = Image.open(filePath)
             blurImage = OriImage.filter(ImageFilter.GaussianBlur(radius=40))
-            
-            blurImage.save(filePath)
+
+            if not os.path.exists(os.path.dirname(outputPath)):
+                os.makedirs(os.path.dirname(outputPath))
+
+            blurImage.save(outputPath)
 
 def copy_files():
     if not os.path.exists('repack-safe/pk3'):
@@ -33,12 +38,15 @@ def copy_files():
 if __name__ == "__main__":
     copy_files()
 
+    if not os.path.exists('repack-safe/nude-textures'):
+        os.makedirs('repack-safe/nude-textures')
+
     if not os.path.exists('repack-safe/safe-textures'):
         os.makedirs('repack-safe/safe-textures')
 
-    find_naughty_images.main('repack-safe/pk3', 'repack-safe/safe-textures', SENSITIVITY)
+    find_naughty_images.main('repack-safe/pk3', 'repack-safe/nude-textures', SENSITIVITY)
 
-    blur_folder('repack-safe/safe-textures')
+    blur_folder('repack-safe/nude-textures', 'repack-safe/safe-textures')
 
     with zipfile.ZipFile('repack/zzz-safe-textures.pk3', 'w') as zip:
         for subdir, dirs, files in os.walk('repack-safe/safe-textures'):
